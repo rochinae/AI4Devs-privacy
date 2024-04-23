88d0e0b67869e619b828f974509b1586505fc104
Enrique Rochina <enrique.rochina@gmail.com>
2024-04-23 19:22:09 +0200

    2nd version

--- privacy-vault/dpv-team-9/server.js
+++ privacy-vault/dpv-team-9/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// Endpoint POST /anonymize
+app.post('/anonymize', (req, res) => {
+    const message = req.body.message;
+    const anonymizedMessage = anonimizar(message);
+    res.json({ anonymizedMessage });
 });
 
+// Endpoint POST /deanonymize
+app.post('/deanonymize', (req, res) => {
+    const message = req.body.message;
+    const deanonymizedMessage = desanonimizar(message);
+    res.json({ deanonymizedMessage });
+});
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
